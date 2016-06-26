eb81a13f65a2407d4a741e61756f1debe39c2cb0
Ivan Liu <Ivan Liu>
2016-06-26 21:13:49 +1000

    documenting

--- ivanr/R/hello.R
+++ ivanr/R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,7 +12,24 @@
 #   Build and Reload Package:  'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
+#   Shortcut Reference:        'Alt + Shift + K'
+#   Re-load the code:          'Ctrl + Shift + L'
+#   Jump to a function:        'F2 / Ctrl + .'
+#   Convert roxygen comments   'Ctrl + Shift + D' devtools::document()
+#   Reflow comments            'Ctrl + Shift + /'
 hello <- function() {
   print("Hello, world!")
+}
+
+
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+  x + y
 }
---
+++ ivanr/R/ivanr.R
@@ -0,0 +1,11 @@
+#' ivanr: A package for computating the notorious bar statistic.
+#'
+#' The ivanr package provides three categories of important functions:
+#' ivanr, bar and baz.
+#'
+#' @section Foo functions:
+#' The ivanr functions ...
+#'
+#' @docType package
+#' @name ivanr
+NULL
